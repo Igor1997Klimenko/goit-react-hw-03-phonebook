c414f995c13c4b438e2f0250d47f899022150c0c
Igor1997Klimenko <Igorek.klimuch.97@gmail.com>
2022-02-11 15:52:33 +0200

    added style

--- src/Component/Container/Container.jsx
+++ src/Component/Container/Container.jsx
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types'
+import s from './Container.module.css'
 
 const Container = ({children}) => {
-    return <div>{children}</div>
+    return <div className={s.Container}>{children}</div>
 }
 
 Container.propTypes = {
